4351e7ee092e188ca6357e8f15781819014d245e
soeunparkk <soeunpark13@gmail.com>
2023-11-03 09:46:52 +0900

    Feat: "SlowTower까지 진행"

--- 2023_2_Project_B/Project_B/Assets/Scrpits/TowerSysetm/EnemyController.cs
+++ 2023_2_Project_B/Project_B/Assets/Scrpits/TowerSysetm/EnemyController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed; //적 이동속도    
+    public float moveSpeed = 1.0f;
+    public float speedMod = 1.0f;
+    public float timeSinceStart = 0.0f;
+                          
+
     private MonsterPath thePath;        //패스정보 값
     private int currentPoint;           //패스 위치 커서값
     private bool reachedEnd;            //도달 완료검사 bool 값
+
+    private bool modEnd = true;
+
     void Start()
     {
         if (thePath == null)                //시작할때 Path 가 없으면
@@ -17,12 +24,23 @@ public class EnemyController : MonoBehaviour
     }
     void Update()
     {
+        if(modEnd == false)
+        {
+            timeSinceStart -= Time.deltaTime;
+
+            if(timeSinceStart <= 0.0f)
+            {
+                speedMod = 1.0f;
+                modEnd = true;
+            }
+        }
+
         if (reachedEnd == false)    //도달 완료가 아닐 경우
         {
             transform.LookAt(thePath.points[currentPoint]); //지금 위치 커서값을 향해서 본다.
 
             transform.position = Vector3.MoveTowards(transform.position
-                , thePath.points[currentPoint].position, moveSpeed * Time.deltaTime);
+                , thePath.points[currentPoint].position, moveSpeed * Time.deltaTime * speedMod);
 
             //나와 패스 포인트 위치의 거리를 계산해서 0.01 이하일경우 도착
             if(Vector3.Distance(transform.position, thePath.points[currentPoint].position) < 0.01f)
@@ -35,5 +53,12 @@ public class EnemyController : MonoBehaviour
                 }                
             }
         }
+    }
+
+    public void SetMode(float value)
+    {
+        modEnd = false;
+        speedMod = value;
+        timeSinceStart = 2.0f;
     }
 }
